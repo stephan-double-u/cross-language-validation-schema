cef8b44bbe11e065ce6dca1e79e77d2ad8c4e208
stephan-double-u <stephanwack@yahoo.de>
2022-03-17 13:05:50 +0100

    Schema test with json-schema-validator

---
+++ src/test/java/de/swa/clv/schema/SchemaValidationTest.java
@@ -0,0 +1,48 @@
+package de.swa.clv.schema;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
+
+// Kudos to https://www.javatpoint.com/json-validator-java
+public class SchemaValidationTest {
+
+    @Test
+    public void validateExampleJson() throws IOException {
+
+        JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Path schemaPath = Paths.get("src/main/resources/cross-language-validation-schema.json");
+        Path jsonPath = Paths.get("src/test/resources/schema-example.json");
+        try (
+                InputStream schemaStream = Files.newInputStream(schemaPath);
+                InputStream jsonStream = Files.newInputStream(jsonPath)
+        ) {
+            JsonSchema schema = schemaFactory.getSchema(schemaStream);
+            JsonNode json = objectMapper.readTree(jsonStream);
+
+            Set<ValidationMessage> validationMsgs = schema.validate(json);
+
+            if (!validationMsgs.isEmpty()) {
+                validationMsgs.forEach(vm -> System.out.println(vm.getMessage()));
+            }
+            assertTrue(validationMsgs.isEmpty());
+        }
+    }
+}
